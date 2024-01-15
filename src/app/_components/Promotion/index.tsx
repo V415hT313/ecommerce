1fec1291160382a6ed8f0642eaca87fdb34a20f3
V415hT313 <vashu2506rkt@gmail.com>
2024-01-15 23:45:18 +0530

    ---

---
+++ src/app/_components/Promotion/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import classes from './index.module.scss'
+
+const Promotion = () => {
+  return (
+    <section className={classes.Promotion}>
+        <div className={classes.textBox}>
+            <h3 className={classes.title}>Deals of the Month</h3>
+            <p>
+              Get ready for a shopping experirnce like never before with our Deals of the Month! Every purchase comes with exclusive perks and offers, making this purchase comes with exclusive perks and offers, making this month a celebration of savvy choices and amazing deals. Don't miss out!
+            </p>
+        </div>
+    </section>
+  )
+}
+
+export default Promotion
